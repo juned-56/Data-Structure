0a716e18daa87528cb7510df5e96b7180757f1ad
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-05 21:27:28 -0700

    Create Maxxoroftwonumbersinanarray.java

---
+++ Maxxoroftwonumbersinanarray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+class Maxxor
+{
+    static int maxxor(int arr[], int n)
+    {
+        int Mxor = 0;
+        for(int i = 0; i < n; i++)  //Calculating Xor of eahc pair
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                Mxor = Math.max(Mxor, arr[i] ^ arr[j]);
+            }
+        }
+        return Mxor;
+
+    }
+public static void main(String[] args)
+{   
+    int n;
+    int arr[] = new int[50];
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of Array: ");
+    n = sc.nextInt();
+    System.out.println("enter the "+n+" Elements: ");
+    for(int i = 0; i < n; i++)
+    {
+        arr[i] = sc.nextInt();
+    }
+    System.out.println(maxxor(arr, n));
+    
+}
+}

481f22c86506aacedf6844e82a58a8e1fa25660b
Juned Khan <73770571+juned-56@users.noreply.github.com>
2021-05-08 08:56:11 -0700

    Create Subarraysum.java

---
+++ Subarraysum.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+class Problem
+{
+    public static void main(String[] args)
+    {
+        int n;
+        int arr[] = new int[50];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        n = sc.nextInt();
+        System.out.println("Enter the "+n+" Values: ");
+        for(int i  = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+
+        }
+        System.out.println("The array is: ");
+        for(int i  = 0; i < n; i++)
+        {
+            System.out.println(arr[i]);
+        }
+        System.out.println("Maximum subarray sum is: "+maxSubArraySum(arr));
+    }
+    static int maxSubArraySum(int arr[])
+    {
+        int size = arr.length;
+        int max = Integer.MIN_VALUE, max_end = 0;
+        for(int i = 0; i < size; i++)
+        {
+            max_end = max_end + arr[i];
+            if(max < max_end)
+            max = max_end;
+        if(max_end < 0)
+        max_end = 0;
+
+        }
+        return max;
+    }
+}

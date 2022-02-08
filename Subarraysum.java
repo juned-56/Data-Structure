10e6ffd7e727e28c377ab47edf5d6223536c8fe2
Juned Khan <73770571+juned-56@users.noreply.github.com>
2022-02-08 19:17:09 +0530

    Update Subarraysum.java

--- Subarraysum.java
+++ Subarraysum.java
@@ -4,11 +4,10 @@ class Problem
 {
     public static void main(String[] args)
     {
-        int n;
-        int arr[] = new int[50];
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array: ");
-        n = sc.nextInt();
+       int n = sc.nextInt();
+        int arr[] = new int[n];
         System.out.println("Enter the "+n+" Values: ");
         for(int i  = 0; i < n; i++)
         {
